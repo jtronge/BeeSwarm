aa5a3bc3074966d10cb88ac34878ea3cb6e95f81
Jake Tronge <jtronge3@gmail.com>
2021-12-01 10:29:57 -0500

    Add port number

--- scripts/ci-workflow.sh
+++ scripts/ci-workflow.sh
@@ -85,7 +85,7 @@ ch-builder2tar -b ch-image ffmpeg $CONTAINER_ARCHIVE)
 
 cd $CWD/workflows
 tar -cf clamr-wf.tgz clamr-wf/
-python ../ci-client.py
+python ../ci-client.py 8933
 
 # Start all components
 #beeflow --sched
