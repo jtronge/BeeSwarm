2bd01cb3e5a6f27d7bca30e90a7d0b8fbbe731ab
Jake Tronge <jtronge3@gmail.com>
2021-11-30 21:13:42 -0500

    Add ci-client program

---
+++ ci-client.py
@@ -0,0 +1,70 @@
+"""Special client for interacting with BEE in a CI environment."""
+import sys
+import os
+import subprocess
+import time
+import requests
+import shutil
+
+
+PORT = 5005
+
+
+def launch(argv):
+    """Launch a command in a subprocess."""
+    pid = os.fork()
+    if pid == 0:
+        os.execv(shutil.which(argv[0]), argv)
+    return pid
+
+
+class BEEManager:
+    """Class for starting and managing the BEE components."""
+
+    def __init__(self):
+        """BEE manager constructor."""
+        self.sched = None
+        self.wfm = None
+        self.tm = None
+
+    def start(self):
+        """Start all the components."""
+        self.sched = launch(['python', '-m', 'beeflow.scheduler.scheduler'])
+        time.sleep(3)
+        self.wfm = launch(['python', '-m', 'beeflow.wf_manager'])
+        self.tm = launch(['python', '-m', 'beeflow.task_manager'])
+        time.sleep(3)
+
+    def run_workflow(self, workflow_path, main_cwl, yaml):
+        """Execute a workflow."""
+        url = 'http://127.0.0.1:{}/{}/'.format(PORT, 'bee_wfm/v1/jobs')
+        files = {
+            'wf_name': 'test'.encode(),
+            'wf_filename': 'some_wfl.tgz',
+            'workflow': open(workflow_path, 'rb'),
+            'main_cwl': main_cwl, 
+            'yaml': yaml,
+        }
+        resp = requests.post(url, files=files)
+        assert resp.ok
+        wf_id = resp.json()['wf_id']
+
+        # Now start it
+        resp = requests.post(url + wf_id, json={'wf_id': wf_id})
+        assert resp.ok
+
+        # Now poll the WFM until the workflow is done
+        # TODO: There should be a better way to do this
+
+
+def main():
+    bee = BEEManager()
+    bee.start()
+
+    bee.run_workflow('clamr-wf.tgz', 'clamr_wf.cwl', 'clamr_job.yml')
+    # Now start workflows as configured
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- scripts/ci-workflow.sh
+++ scripts/ci-workflow.sh
@@ -83,21 +83,25 @@ cd ../ffmpeg
 ch-image build --force -t ffmpeg .
 ch-builder2tar -b ch-image ffmpeg $CONTAINER_ARCHIVE)
 
-# Start all components
-beeflow --sched
-sleep 5
-python -m beeflow.wf_manager &
-python -m beeflow.task_manager &
+cd $CWD/workflows
+tar -cf clamr-wf.tgz clamr-wf/
+python ci-client.py
 
-sleep 15
+# Start all components
+#beeflow --sched
+#sleep 5
+#python -m beeflow.wf_manager &
+#python -m beeflow.task_manager &
+
+#sleep 15
 
 # Now start the CLAMR workflow
-echo "Before package"
-beeflow-client --cli package $CWD/workflows/clamr-wf
-echo "Before submit"
-WF_ID=`beeflow-client --cli submit clamr-test-run ./clamr-wf.tgz clamr_wf.cwl -y clamr_job.yml | cut -d'=' -f2`
-echo "WF_ID=" $WF_ID
-beeflow-client --cli start $WF_ID
-echo "After start"
+#echo "Before package"
+#beeflow-client --cli package $CWD/workflows/clamr-wf
+#echo "Before submit"
+#WF_ID=`beeflow-client --cli submit clamr-test-run ./clamr-wf.tgz clamr_wf.cwl -y clamr_job.yml | cut -d'=' -f2`
+#echo "WF_ID=" $WF_ID
+#beeflow-client --cli start $WF_ID
+#echo "After start"
 
 sleep 100
