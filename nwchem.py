083773db32a8cc19071047ea1ba3ada2bc512d70
Jake Tronge <jtronge@kent.edu>
2022-02-24 13:25:13 -0500

    change order of graphs

--- nwchem.py
+++ nwchem.py
@@ -4,10 +4,10 @@ import json
 plt.style.use('./matplotlib-styles/paper.mplstyle')
 
 files = [
-    'results/nwchem-run-05aafc87223af82f58865d8b0f924dabd1adacbc.json',
-    'results/nwchem-run-519b710bd09ebe3c10703293093d3cf70ce44333.json',
-    'results/nwchem-run-adab52acbbc0fb65b35856abf53937360bb52026.json',
     'results/nwchem-run-f29685d5bde59ac9c7f71b7ce6193720aaba1a8b.json',
+    'results/nwchem-run-05aafc87223af82f58865d8b0f924dabd1adacbc.json',
+    'results/nwchem-run-adab52acbbc0fb65b35856abf53937360bb52026.json',
+    'results/nwchem-run-519b710bd09ebe3c10703293093d3cf70ce44333.json',
 ]
 commits = []
 times = []
